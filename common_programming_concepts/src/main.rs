52c9b79c041478c56e541915247bb4a1e69fda8b
DerekAlmanza <DerekAlmanza@ciencias.unam.mx>
2019-12-26 18:02:22 -0600

    The shadowing concept is added

--- common_programming_concepts/src/main.rs
+++ common_programming_concepts/src/main.rs
@@ -1,5 +1,7 @@
 fn main() {
+    
     // Variables and mutability.
+    // If we put let x = 5; the code will fail because later the x value will change.
     let mut x= 5;
     println!("The value of x is {}", x);
     x = 6;
@@ -10,6 +12,12 @@ fn main() {
     // inserted in numeric literals to improve readability.
     const MAX_POINTS: u32 = 100_000;
 
-    //Shadowing. Use the same variable name repeatdly for different uses.
+    // Shadowing. Use the same variable name repeatdly for different uses.
+    let y:i32 = 6; // Here we specify the data type because otherwise it wouldn't compile the line 20. 
+    println!("The value of y is {}", y);
+    let y = y + 2;
+    println!("The value of y is {}", y);
+    let y = y.pow(2);
+    println!("The value of y is {}", y);
     
 }
---
+++ common_programming_concepts/target/debug/deps/common_programming_concepts-c85b5d5ef6c84363.d
@@ -0,0 +1,5 @@
+/home/derek/Learning_Rust/common_programming_concepts/target/debug/deps/common_programming_concepts-c85b5d5ef6c84363: src/main.rs
+
+/home/derek/Learning_Rust/common_programming_concepts/target/debug/deps/common_programming_concepts-c85b5d5ef6c84363.d: src/main.rs
+
+src/main.rs:
